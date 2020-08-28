4531be429891b77ce71e87934c41c9524fb2cba1
maukii <maunokiiskinen@kamk.fi>
2020-08-29 00:09:02 +0300

    Addded hands to XrRig and added a surfboard model

---
+++ VrPrototyping/Assets/Scripts/HandPresence.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    [SerializeField] private bool showController = false;
+    [SerializeField] private InputDeviceCharacteristics controllerCharacteristics;
+    [SerializeField] private List<GameObject> controllerPrefabs;
+    [SerializeField] private GameObject handModelPrefab;
+
+    private InputDevice targetDevice;
+    private GameObject spawnedController;
+    private GameObject spawnedHandModel;
+    private Animator handAnimator;
+
+
+    private void Awake()
+    {
+        TryInitialize();
+    }
+
+    private void Update()
+    {
+        if(!targetDevice.isValid)
+        {
+            TryInitialize();
+            return;
+        }
+
+        if (showController) 
+            return;
+
+        UpdateHandAnimation();
+    }
+
+    private void TryInitialize()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+            if (prefab)
+            {
+                spawnedController = Instantiate(prefab, transform);
+            }
+            else
+            {
+                Debug.LogError("Did not find corresponding controller model, using default instead");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            }
+
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+            handAnimator = spawnedHandModel.GetComponent<Animator>();
+
+            spawnedHandModel.SetActive(!showController);
+            spawnedController.SetActive(showController);
+        }
+    }
+
+    private void UpdateHandAnimation()
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+            handAnimator.SetFloat("Trigger", triggerValue);
+        else
+            handAnimator.SetFloat("Trigger", 0f);
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+            handAnimator.SetFloat("Grip", gripValue);
+        else
+            handAnimator.SetFloat("Grip", 0f);
+    }
+}
